a7167aa98ae5ff48b5604ab151778dc3354e3e5b
Supaishichikin <jordan.toko@outlook.fr>
2023-03-15 17:18:19 +0100

    docker db hello world route
    hello world route and updtade readme and dependencies. Make docker database

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ create a virtual environment and install the requirements.txt file
 1. `python -m venv env`
 2. on mac `source env/bin/activate`
 3. on windows `source env/bin/activate`
-4. `pip install --no-cache-dir --upgrade -r /backend/requirements.txt`
+4. `pip install --no-cache-dir --upgrade -r requirements.txt`
 
 then run the project at the root of the projet using this command:
 `flask run`
--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DATABASE'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+mysql = MySQL(app)
 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/')
+@app.route('/api')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT DISTINCT STATUS FROM `bureau_balance`')
+    result = cur.fetchall()
+    cur.close()
+    return jsonify(result)
 
 
 if __name__ == '__main__':
